6872d09836e678988b17acf1ae452a9bc0e39488
fahad <bsse1105@iit.du.ac.bd>
2023-01-02 12:47:53 +0600

    done with graphics

--- src/pages/Graphic_design/graphic_details.jsx
+++ src/pages/Graphic_design/graphic_details.jsx
@@ -6,7 +6,7 @@
     import Service from "./gservices/service";
     import GalleryImage from "./GalleryImage";
     import CommonCtaArea from "../../components/CommonCtaArea/CommonCtaArea";
-
+    import Faq from "../HomeTwo/HomeTwoFaq/HomeTwoFaq";
     import "swiper/css";
     import "swiper/css/pagination";
     import "swiper/css/navigation";
@@ -14,6 +14,28 @@
     import { Pagination, Navigation } from "swiper";
 
     function graphic_details() {
+        const arrayFaq = [
+            {
+              question: "Don't have hosting right now. Can we start?",
+              solution:
+                "Absolutely! We would start the development on our development server. Once the website build is complete, we will transfer to your hosting. You can also have us host your website for a minimal monthly fee.",
+            },
+            {
+              question: "What do you need to get started?",
+              solution:
+                "You will have to provide us with : Project brief which describes the Main Idea and specific details, pictures,logo, content, WordPress login details. If you missing some of the requirements just contact us and we will help you with everything. If you need help in any of them, Contact us.",
+            },
+            {
+              question: "Will you maintain my site for me?",
+              solution:
+                "Of course, we can help you out with proper and professional website maintenance in addition to development as it supports in achieving stabilized growth, keeping your business inclined.",
+            },
+            {
+              question: "Will my website be mobile-friendly?",
+              solution:
+                "It also increases the number of visitors, rankings in search engines.Yes, we ensure to develop a responsive website that would make your visitors appealing and deliver an optimized browsing experience.",
+            },
+          ];
     const images = [
         "/assets/img/graphics/1.png",
         "/assets/img/graphics/2.png",
@@ -45,35 +67,77 @@
         {
         title: "Logo & Brand Identity",
         src: "assets/img/graphics_page/Logo _ Brand Identity_B_2x.png",
-        item1: "Logo Design",
+        item: ["Logo Design", "Brand Style Guides", "Font & Typography"],
         },
         {
         title: "Web & App Design",
         src: "assets/img/graphics_page/Web _ App_2x.png",
+        item: [
+            "Website Design",
+            "App Design",
+            "Landing Page Design",
+            "UX Design",
+            "UI Design",
+        ],
         },
         {
         title: "Art & Illustration",
         src: "assets/img/graphics_page/Art _Illustration_2x.png",
+        item: [
+            "Illustration",
+            "NFT Art",
+            "Cartoon & Comics",
+            "Portraits & B Caricatures",
+        ],
         },
         {
         title: "Marketing Design",
         src: "assets/img/graphics_page/Social_Media_2x.png",
+        item: [
+            "Social Media Design",
+            "Email Design",
+            "Web Banners",
+            "Signage Design",
+        ],
         },
         {
         title: "Print Design",
         src: "assets/img/graphics_page/print_design_2x.jpeg",
+        item: [
+            "Flyer Design",
+            "Brochure Design",
+            "Poster Design",
+            "Catalog Design",
+            "Menu Design",
+            "Invitation Design",
+        ],
         },
         {
         title: "Fashion & Merchandise",
         src: "assets/img/graphics_page/Fashion_Merchandise_2x.png",
+        item: ["Fashion Design", "T-shirt & Merchandise", "Jewelry Design"],
         },
         {
         title: "Visual Design",
         src: "assets/img/graphics_page/Visual Design_2x.png",
+        item: [
+            "Image Design",
+            "Presentation Design",
+            "Infographic Design",
+            "Vector Tracing",
+            "Resume Design",
+        ],
         },
         {
         title: "Packaging & Covers",
         src: "assets/img/graphics_page/Packaging_Labels_2x.png",
+        item: [
+            "Packaging & Level Design",
+            "Book Design",
+            "Album Cover Design",
+            "Podcast Cover Art",
+            "Car Wraps",
+        ],
         },
     ];
     const service = [
@@ -150,21 +214,21 @@
 
             <h2
                 className=" lg:leading-relaxed hidden md:block sm:text-[28px] md:text-[32px] lg:text-[36px]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                    text-white text-center"
+                            text-white text-center"
             >
                 Imagination Bleeds From <br />
                 The Pen Of Creativity
             </h2>
             <h3
                 className="leading-1 hidden sm:block md:hidden text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                text-white text-center"
+                        text-white text-center"
             >
                 Imagination Bleeds From <br />
                 The Pen Of Creativity
             </h3>
             <h4
                 className="leading-1  sm:hidden text-lg  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                text-white text-center"
+                        text-white text-center"
             >
                 Imagination Bleeds From <br />
                 The Pen Of Creativity
@@ -204,15 +268,16 @@
             <div className="px-[40px]">
             <h3 className="py-[20px]">Our graphics & design Services</h3>
             <div className="flex  justify-between flex-wrap ">
-                {services.map(({ src, title }) => {
+                {services.map(({ src, title, item }) => {
                 return (
                     <div className="  w-[100%] sm:w-[40%] md:w-[30.33%] lg:w-[20%] flex-col items-center   m-2 rounded-md  ">
                     <img src={src} alt="vector" className="w-full  rounded-lg " />
                     <h4 className=" flex-[0_0_auto] pt-[20px]">{title}</h4>
+
                     <ul>
-                        <li className="py-1">Logo Design</li>
-                        <li className="py-1">Brand Style Guides</li>
-                        <li className="py-1">Font & Typography</li>
+                        {item.map((i) => {
+                        return <li className="py-2">{i}</li>;
+                        })}
                     </ul>
                     </div>
                 );
@@ -254,7 +319,7 @@
             </div>
             </div>
         </div>
-
+        <Faq arrayFaq={arrayFaq}/>
         <CommonCtaArea />
         <HomeTwoFooter />
         </>
